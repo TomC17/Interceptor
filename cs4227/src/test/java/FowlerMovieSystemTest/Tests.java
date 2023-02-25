67e4b9ea2cda4c85284fcb4ad016f8158058cc77
Tomc17 <tommiecarey17@gmail.com>
2023-02-25 19:50:16 +0000

    Update Tests.java

--- cs4227/src/test/java/FowlerMovieSystemTest/Tests.java
+++ cs4227/src/test/java/FowlerMovieSystemTest/Tests.java
@@ -1,6 +1,8 @@
 package test.java.FowlerMovieSystemTest;
 
+import main.java.FowlerMovieSystem.Context.CustomerContext;
 import main.java.FowlerMovieSystem.Customer;
+import main.java.FowlerMovieSystem.Dispatcher.Dispatcher;
 import main.java.FowlerMovieSystem.Interceptor.ConsoleLoggingInterceptor;
 import main.java.FowlerMovieSystem.Interceptor.Interceptor;
 import main.java.FowlerMovieSystem.Movie;
@@ -13,54 +15,100 @@ public class Tests {
     ConsoleLoggingInterceptor consoleLoggingInterceptor = new ConsoleLoggingInterceptor();
     Interceptor[] interceptors = {consoleLoggingInterceptor};
 
-
-
-    Customer customer = new Customer("Tester");
     @Test
     public void CustomerTest() {
-        Rental rental1 = new Rental(
-                new Movie("REGULAR", Movie.REGULAR),
-                1
-        );
+        // Create a new customer
+        Customer customer = new Customer("Tester");
+
+        // Add some rentals to the customer
+        Movie movie1 = new Movie("Movie 1", Movie.REGULAR);
+        Rental rental1 = new Rental(movie1, 3);
         customer.addRental(rental1);
-        Rental rental2 = new Rental(
-                new Movie("NEW_RELEASE 1", Movie.NEW_RELEASE),
-                2
-        );
+
+        Movie movie2 = new Movie("Movie 2", Movie.NEW_RELEASE);
+        Rental rental2 = new Rental(movie2, 2);
         customer.addRental(rental2);
-        Rental rental3 = new Rental(
-                new Movie("NEW_RELEASE 2", Movie.NEW_RELEASE),
-                3
-        );
-        customer.addRental(rental3);
-        Rental rental4 = new Rental(
-                new Movie("CHILDREN", Movie.CHILDREN),
-                4
-        );
-        customer.addRental(rental4);
 
-        String expected = """
-                Rental record for Tester
-                \tREGULAR\t2.0
-                \tNEW_RELEASE 1\t6.0
-                \tNEW_RELEASE 2\t9.0
-                \tCHILDREN\t3.0
-                Amount owed is 20.0
-                You earned 6 frequent renter points""";
-
+        // Call the statement method and check the output
+        String expected = "Rental record for Tester\n" +
+                "\tMovie 1\t3.5\n" +
+                "\tMovie 2\t6.0\n" +
+                "Amount owed is 11.0\n" +
+                "You earned 3 frequent renter points";
         assertEquals(expected, customer.statement());
-        System.out.println(customer.statement());
+
+        //now we want to test the interceptor
+        //first we create the dispatcher
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.registerInterceptor(consoleLoggingInterceptor);
+
+
+        // Create a new customer with context
+        Customer customer2 = new Customer("Tess Tue");
+        //now we want to add some rentals to the customer
+        Movie movie3 = new Movie("Movie 3", Movie.REGULAR);
+        Rental rental3 = new Rental(movie3, 3);
+        customer2.addRental(rental3);
+
+        Movie movie4 = new Movie("Movie 4", Movie.NEW_RELEASE);
+        Rental rental4 = new Rental(movie4, 2);
+        customer2.addRental(rental4);
+
+        Movie movie5 = new Movie("Movie 5", Movie.CHILDREN);
+        Rental rental5 = new Rental(movie5, 6);
+        customer2.addRental(rental5);
+
+        CustomerContext context = new CustomerContext(customer2);
+        //now we want to create a stringbuilder to store the output of various methods
+        StringBuilder resultBuilder = new StringBuilder();
+        //now we want to call the dispatcher to dispatch the context
+        dispatcher.dispatch(context);
+        //now we want to get the output from the context
+        resultBuilder.append(context.getName());
+        resultBuilder.append(context.getRentalsAmount());
+        resultBuilder.append(context.getTotalFrequentRenterPoints());
+        resultBuilder.append(context.getTotalCharge());
+        //now we want to check the output
+
+        String expectedInterceptor = "Tess Tue3445.5";
+        assertEquals(expectedInterceptor, resultBuilder.toString());
     }
 
     @Test
     public void NoCustomersTest() {
-        String expected = "" +
-                "Rental record for Tester\n" +
-                "Amount owed is 0.0\n" +
-                "You earned 0 frequent renter points";
+        // Create a new customer with no rentals
+        Customer customer = new Customer("Empty");
 
+        // Call the statement method and check the output
+        String expected = """
+                Rental record for Empty
+                Amount owed is 0.0
+                You earned 0 frequent renter points""";
         assertEquals(expected, customer.statement());
-        System.out.println(customer.statement());
+
+        //now we want to test the interceptor
+        //first we create the dispatcher
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.registerInterceptor(consoleLoggingInterceptor);
+
+        // Create a new customer with context
+        Customer customer2 = new Customer("Empty ");
+        CustomerContext context;
+        context = new CustomerContext(customer2);
+        //now we want to create a stringbuilder to store the output of various methods
+        StringBuilder resultBuilder = new StringBuilder();
+        //now we want to call the dispatcher to dispatch the context
+        dispatcher.dispatch(context);
+        //now we want to get the output from the context
+        resultBuilder.append(context.getName());
+        resultBuilder.append(context.getRentalsAmount());
+        resultBuilder.append(context.getTotalFrequentRenterPoints());
+        resultBuilder.append(context.getTotalCharge());
+        //now we want to check the output
+
+        String expectedInterceptor = "Empty 000.0";
+        assertEquals(expectedInterceptor, resultBuilder.toString());
+
+
     }
 }
-
